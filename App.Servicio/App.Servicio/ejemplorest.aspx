c5bcd7e5f99829df1f18cff9b0d53ff08eae3fb7
Christian velasquez <crisvelasqiez02@gmail.com>
2015-03-29 23:24:50 -0500

    Este es mi tercer cambio

---
+++ App.Servicio/App.Servicio/ejemplorest.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ejemplorest.aspx.cs" Inherits="App.Servicio.ejemplorest" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        <br />
+        <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+        <br />
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" style="height: 26px" Text="Sumar" />
+        <br />
+        <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ App.Servicio/App.Servicio/ejemplorest.aspx.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace App.Servicio
+{
+    public partial class ejemplorest : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            String a = TextBox1.Text;
+            String b = TextBox2.Text;
+
+            String url = "http://localhost:5649/SvcOperaciones.svc/Suma/{0}/{1}";
+
+
+            String newurl = string.Format(url, a, b);
+            WebRequest req = WebRequest.Create(newurl);
+            req.Method = "GET";
+            req.ContentType = "application/json; charset=utf-8";
+            try
+            {
+                HttpWebResponse resp = req.GetResponse() as HttpWebResponse;
+                if (resp.StatusCode == HttpStatusCode.OK)
+                {
+                    using (Stream respStream = resp.GetResponseStream())
+                    {
+                        StreamReader reader = new StreamReader(respStream, Encoding.ASCII);
+                        TextBox3.Text = reader.ReadToEnd();
+
+                    }
+                }
+                else {
+
+                    Response.Write(string.Format("Status Code : {0}, Status Description: {1} ", resp.StatusCode, resp.StatusDescription));
+                }
+            }
+            catch (Exception ex)
+            {
+                
+                throw ex;
+            }
+    
+        
+        
+        }
+    }
+}
