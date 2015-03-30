7e1507e8a3257204db08a0a17358b1420f419f2a
Christian velasquez <crisvelasqiez02@gmail.com>
2015-03-29 22:40:57 -0500

    És te es mi segundo comit

---
+++ App.Servicio/App.Wcf/ISvcOperaciones.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace App.Wcf
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "ISvcOperaciones" in both code and config file together.
+    [ServiceContract]
+    public interface ISvcOperaciones
+    {
+        [OperationContract]
+        void DoWork();
+
+        [OperationContract]
+        [WebGet(UriTemplate="/Suma/{a}/{b}",ResponseFormat=WebMessageFormat.Json)]
+        string Add(string a,string b);
+    }
+}
---
+++ App.Servicio/App.Wcf/SvcOperaciones.svc.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace App.Wcf
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "SvcOperaciones" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select SvcOperaciones.svc or SvcOperaciones.svc.cs at the Solution Explorer and start debugging.
+    public class SvcOperaciones : ISvcOperaciones
+    {
+        public void DoWork()
+        {
+        }
+
+
+        public string Add(string a, string b)
+        {
+            var s = Convert.ToDouble(a) + Convert.ToDouble(b);
+            return "La Suma es "+ s;
+        }
+    }
+}
